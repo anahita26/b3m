603b7b43d744ffece2093ec782cc6e59b87aeb3f
anahita26 <anahita.darvish@gmail.com>
2025-10-31 15:21:25 +0800

    add shading and tree measures

---
+++ R/IDF_modification.R
@@ -0,0 +1,98 @@
+## The following R script makes all necessary modifications to the original IDF file to prepare it for analysis
+
+# Load relevant libraries
+library(eplusr)
+library(here)
+library(dplyr)
+
+# Load IDF
+use_eplus("C:/EnergyPlusV9-3-0")
+path_idf <- here("data", "idf", "Blk7_V930.idf")
+model <- read_idf(path_idf)
+
+# Remove all output variables and meters
+model$Output_Variable <- NULL
+model$Output_Meter <- NULL
+
+# Add desired output variables
+output_list <- list(
+  Output_Variable = list(
+    key_value = "*",
+    Variable_Name = "Zone Ideal Loads Supply Air Sensible Cooling Energy",
+    Reporting_Frequency = "Monthly"
+  ),
+  Output_Variable = list(
+    key_value = "*",
+    Variable_Name = "Zone Ideal Loads Supply Air Total Cooling Energy",
+    Reporting_Frequency = "Monthly"
+  ),
+  Output_Variable = list(
+    key_value = "*",
+    Variable_Name = "Surface Outside Face Temperature",
+    Reporting_Frequency = "Monthly"
+  ),
+  Output_Variable = list(
+    key_value = "*",
+    Variable_Name = "Surface Inside Face Temperature",
+    Reporting_Frequency = "Monthly"
+  ),
+  Output_Variable = list(
+    key_value = "*",
+    Variable_Name = "Surface Outside Face Conduction Heat Gain Rate",
+    Reporting_Frequency = "Monthly"
+  ),
+  Output_Variable = list(
+    key_value = "*",
+    Variable_Name = "Surface Outside Face Incident Solar Radiation Rate per Area",
+    Reporting_Frequency = "Monthly"
+  ),
+  Output_Meter = list(
+    key_name = "Electricity:Facility",
+    Reporting_Frequency = "Monthly"
+  )
+)
+model$add(output_list)
+
+# Update the surface details to include the roof vegetation
+model$'Material:RoofVegetation'
+roof_tbl <- model$to_table(class = "BuildingSurface:Detailed")
+roof_tbl <- roof_tbl |> 
+  mutate(value = case_when(
+    value == "Exterior Roof" ~ "Green Roof",
+    TRUE ~ value
+  ))
+model$update(roof_tbl)
+
+# Add overhang shading to each window 
+windows <- model$to_table(class = "FenestrationSurface:Detailed")
+windows <- windows |> 
+  filter(value == "Window", field =="Surface Type") |> 
+  group_by(name) 
+
+#|> 
+  #summarise(window_name = first(name))
+
+create_overhangs <- function(model, windows, depth = 0) {
+  for (i in seq_len(nrow(windows))) {
+    win_name <- windows$name[i]
+    new_mat <- list(
+      'Shading:Overhang:Projection' = list(
+        Name = paste0("Overhang_", win_name),
+        'Window or Door Name' = win_name,
+        'Height above Window or Door' = 0.1,
+        'Tilt Angle from Window/Door' = 0,
+        'Left extension from Window/Door Width' = 0,
+        'Right extension from Window/Door Width' = 0,
+        'Depth as Fraction of Window/Door Height' = depth
+      ))
+    model$add(new_mat)
+  }
+  return(model)
+}
+
+model <- create_overhangs(model, windows, depth = 0)
+model$"Shading:Overhang:Projection"
+
+
+# Save updated model
+model$save(here("data", "idf", "model_preprocessed.idf"), overwrite = TRUE)
--- R/analysis.R
+++ R/analysis.R
@@ -6,58 +6,50 @@ library(eplusr)
 library(here)
 library(tidyverse)
 library(dplyr)
-library(data.table)
+library(data.table)                         
 
 # Assign idf and weather file
 use_eplus("C:/EnergyPlusV9-3-0")
-path_idf <- here("data", "idf", "Blk7_V930.idf")
+path_idf <- here("data", "idf", "model_preprocessed.idf")
 model <- read_idf(path_idf)
 path_epw <- here("data", "epw", "SGP_Developed_Site(Blk7).epw")
 epw <- read_epw(path_epw)
 #job <- model$run(epw, dir = tempdir())
 
-# Add desired output variables
-output_list <- list(
-  Output_Variable = list(
-    key_value = "*",
-    Variable_Name = "Zone Ideal Loads Supply Air Sensible Cooling Energy",
-    Reporting_Frequency = "Hourly"
-  )
-)
-
-model$add(output_list)
-model$save(here("data", "idf", "model_preprocessed.idf"), overwrite = TRUE)
-
 # Call parametric tool
 param <- param_job(model, epw)
 
 # Assign scenario variables 
-shading <- c(0, 0.8, 1.5)
+shading <- c(0, 0.5, 1)
 coating <- c(0.7, 0.4, 0.2)
-lai <- c(0, 2.5, 5)
+lai <- c(1, 2.5, 5)
 
-# Create measure for modifying shading
-# List parent walls
-surf_geo <- model$geometry()
-ext_walls <- surf_geo$azimuth() |> 
-  filter(class == "BuildingSurface:Detailed" & 
-           type == "Wall")
-# Filter for only outdoor walls
-# Not sure how to do this yet
+## Create measure for modifying shading
+# Check value
+depth <- model$'Shading:Overhang:Projection'$Overhang_9B19D6$`Depth as Fraction of Window/Door Height`
 
 set_shading <- function(model, shading = NA) {
-  if (is.na(shading)) return(mdoel)
-  shade_tbl <- model$to_table("Shading:Overhang:Projection")
+  # keep original if applicable
+  if (is.na(shading)) return(model)
+  
+  # extract all overhang objects properly
+  shade_tbl <- model$to_table(class = "Shading:Overhang:Projection")
+  
+  # update fraction of overhang shading
   shade_tbl <- shade_tbl |> 
-    mutate(value = ifelse(field == "Height above Window or Door",
+    mutate(value = ifelse(field == "Depth as Fraction of Window/Door Height",
                         sprintf("%0.2f", shading),
                         value))
+  
+  # update model with new overhang fraction
   model$update(shade_tbl)
+  
+  # return modified model
   model
 }
 
-# Create measure to modify coating
-# Extract the correct value
+## Create measure to modify coating
+# Verify the correct value 
 material <- model$'Material'$'M15 150mm heavyweight concrete'$`Solar Absorptance`
 
 set_coating <- function(model, coating = NA) {
@@ -77,30 +69,69 @@ set_coating <- function(model, coating = NA) {
       field == "Solar Absorptance" ~ sprintf("%.2f", coating),
       TRUE ~ value))
   
-  # update absorptance using table
+  # update model with new absorptance
   model$update(coat_tbl)
+  
+  # return modified model
+  model
+}
+
+## Create measure to modify leaf area index
+# Check value
+model$'Material:RoofVegetation'$'green_roof'$`Leaf Area Index`
+
+set_lai <- function(model, lai = NA) {
+  # keep original if applicable
+  if (is.na(lai)) return(model) #What exactly does this mean/ what does it do / when is it applicable?
+  
+  # extract all Material objects properly
+  lai_tbl <- model$to_table(class = "Material:RoofVegetation")
+  
+  # modify the leaf area index field
+  lai_tbl <- lai_tbl |> 
+    mutate(value = case_when(
+      field == "Leaf Area Index" ~ sprintf("%.2f", lai), 
+      TRUE ~ value))
+  
+  # update model with new leaf area index
+  model$update(lai_tbl)
   
   #return modified model
   model
+  
 }
 
 # Test it out
 model_update <- set_coating(model, coating[2])
+model_update_leaf <- set_lai(model, lai[2])
 model_update$Material$`M15 150mm heavyweight concrete`$`Solar Absorptance`
+model_update_leaf$'Material:RoofVegetation'$'green_roof'$`Leaf Area Index`
+model_update_shade <- set_shading(model, shading[2])
+model_update_shade$'Shading:Overhang:Projection'$Overhang_9B19D6$`Depth as Fraction of Window/Door Height`
 
-# Use apply measure
+# Apply measure to model
 param$apply_measure(set_coating,
                     coating,
                     .names = c("Dark", "Medium", "Cool"))
+param$apply_measure(set_lai,
+                    lai,
+                    .names = c("Sparse", "Moderate", "Lush"))
+param$apply_measure(set_shading,
+                    shading,
+                    .names = c("No Shade", "Half Shade", "Full Shade"))
 
 # Retrieve summary of parameter values and model names
 param$cases()
 
-# Run parametric simulations in parallel
+## Run parametric simulations in parallel
 param$run()
 param$status()
 
-# Collect results
+## Collect results
+report <-  param$report_data()
+#Filter by on specific variable and sum over all the zones and time to get the final result of the building
+
+# Need to look deeper into results that are actually affected by the green roof to show success
 param_energy <- param$tabular_data(table_name = "Site and Source Energy", wide = TRUE)[[1L]] |> 
   select(case,
          row_name,
